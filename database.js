3fa55d4ce40460c6e94399f7513b6bb57c0bc792
EL-Yasmina <yasminataif99@gmail.com>
2024-01-12 20:39:11 +0100

    uprdate connexion

--- database.js
+++ database.js
@@ -11,55 +11,75 @@ const dbConfig = {
 };
 //mysqli_connect("sql11.freesqldatabase.com","sql11667198","cdSPCALtRD","sql11667198");
 // Création de la connexion à la base de données
-const connection = mysql.createConnection(dbConfig);
 
 // pour Connexion à la base de données
-connection.connect((err) => {
+function open_database(){
+    const connection = mysql.createConnection(dbConfig);
+    connection.connect((err) => {
+        console.log("4");
+        if (err) {
+            console.error('Erreur de connexion à la base de données : ', err);
+        } else {
+            console.log('Connecté à la base de données MySQL');
+        }
+    });
+    return connection;
+}
 
-    console.log("4");
-    if (err) {
-        console.error('Erreur de connexion à la base de données : ', err);
-    } else {
-        console.log('Connecté à la base de données MySQL');
-    }
-});
 
 // ici la Fonction pour récupérer les commandes depuis la base de données
 function db_getCommandes(callback) {
+    const connection= open_database();
     const query =
         'SELECT commande.id, commande.date, commande.quantite, commande.statut, produit.nom AS nomProduit, client.nom AS nomClient ' +
         'FROM commande ' +
         'INNER JOIN client ON client.id = commande.id_client ' +
         'INNER JOIN produit ON produit.id = commande.id_produit order by commande.id DESC';
 
-    connection.query(query, (err, results) => callback(results));
+    connection.query(query, (err, results) => {
+        callback(results);
+        connection.end();
+    });        
 }
 
 function db_getProduits(callback) {
+    const connection= open_database();
     const query = 'SELECT * FROM produit order by id DESC';
-    connection.query(query, (err,results) => callback(results));
+    connection.query(query, (err,results) => {
+        callback(results);
+        connection.end();
+    });        
 }
 
 function db_deleteProduit(produitId, callback) {
+    const connection= open_database();
     const deleteCommandesQuery = 'DELETE FROM commande WHERE id_produit = ?';
     connection.query(deleteCommandesQuery, [produitId], (err, results) => {
         const db_deleteProduitQuery = 'DELETE FROM produit WHERE id = ?';
-        connection.query(db_deleteProduitQuery, [produitId], () => callback());        
+        connection.query(db_deleteProduitQuery, [produitId], () => {
+            callback();
+            connection.end();
+        });        
     });
 }
 
 
 
 function db_ajouterProduit(produit, callback) {
+    const connection= open_database();
     const query =
         'INSERT INTO produit (nom, quantite, image, solde, prix, description) ' +
         'VALUES (\'' + produit.nom + '\', ' + produit.quantite + ', ' + produit.image + ', ' + produit.solde + ', ' + produit.prix + ', \'' + produit.description + '\');';
     
-    connection.query(query, () => callback());
+    connection.query(query, () => {
+        callback();
+        connection.end();
+    });
     produit.image = null;
 }
 
 function db_modifierProduit(produit, callback) {
+    const connection= open_database();
     console.log('db_modifierProduit', produit)
     let image = "";
     if(produit.image != null){
@@ -76,18 +96,29 @@ function db_modifierProduit(produit, callback) {
         'WHERE id = ' + produit.id;
 
         console.log('db_modifierProduit', query)
-    connection.query(query, () => callback());
+    connection.query(query, () => {
+        callback();
+        connection.end();
+    });
     produit.image = null;
 }
 
 function db_modifierStatutCommande(idCommande, statut, callback) {
+    const connection= open_database();
     const query = 'UPDATE commande SET statut = ? WHERE id = ?';
-    connection.query(query, [statut, idCommande], () => callback());
+    connection.query(query, [statut, idCommande], () => {
+        callback();
+        connection.end();
+    });
 }
 
 function db_getProduitById(produitId, callback) {
+    const connection= open_database();
     const query = 'SELECT * FROM produit WHERE id = ?';
-    connection.query(query, [produitId], (err, results) => callback(results[0]));
+    connection.query(query, [produitId], (err, results) => {
+        callback(results[0]);
+        connection.end();
+    });
 }
 module.exports = {
     db_modifierStatutCommande, db_getCommandes, db_getProduits, db_deleteProduit, db_ajouterProduit, db_modifierProduit,db_getProduitById
